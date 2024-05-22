e928bcbf472b7536c1eb1591c908d25f4dbfdbe5
main-main28 <161801900+main-main28@users.noreply.github.com>
2024-05-22 09:12:56 +0700

    Add files via upload

--- update.sh
+++ update.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+
 cd /usr/local/
 rm -rf sbin
 rm -rf /usr/bin/enc
 cd
 mkdir /usr/local/sbin
+# Get the current date from Google's server
 dateFromServer=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
-biji=`date +"%Y-%m-%d" -d "$dateFromServer"`
-red() { echo -e "\\033[32;1m${*}\\033[0m"; }
-clear
+biji=$(date +"%Y-%m-%d" -d "$dateFromServer")
+
+# Function to print text in red color
+red() { 
+    echo -e "\\033[32;1m${*}\\033[0m"
+}
+
+# Function to display a loading bar
 fun_bar() {
     CMD[0]="$1"
     CMD[1]="$2"
@@ -20,11 +27,14 @@ fun_bar() {
     tput civis
     echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     while true; do
-        for ((i = 0; i < 36; i++)); do # Menambah jumlah iterasi perulangan untuk membuat animasi lebih panjang
-            echo -ne "\033[0;32m>" # Menggunakan karakter panah dalam animasi
+        for ((i = 0; i < 18; i++)); do
+            echo -ne "\033[0;32m#"
             sleep 0.1s
         done
-        [[ -e $HOME/fim ]] && rm $HOME/fim && break
+        if [[ -e $HOME/fim ]]; then
+            rm $HOME/fim
+            break
+        fi
         echo -e "\033[0;33m]"
         sleep 1s
         tput cuu1
@@ -32,11 +42,12 @@ fun_bar() {
         echo -ne "  \033[0;33mPlease Wait Loading \033[1;37m- \033[0;33m["
     done
     echo -e "\033[0;33m]\033[1;37m -\033[1;32m OK !\033[1;37m"
-    echo -e "\033[32mJangan lupa ngopi\033[0m" # Menambahkan teks "Jangan lupa ngopi" dengan warna hijau
     tput cnorm
 }
+
+# Function to download, extract, and move files
 res1() {
-    wget --no-check-certificate https://raw.githubusercontent.com/main-main28/main-main28/main/limit/menu.zip
+    wget --no-check-certificate https://rmblvpn.cloud/limit/menu.zip
     unzip menu.zip
     chmod +x menu/*
     enc menu/*
@@ -45,18 +56,33 @@ res1() {
     rm -rf menu.zip
     rm -rf update.sh
 }
+
+# Ensure netfilter-persistent is running
 netfilter-persistent
+
+# Clear the screen
 clear
+
+# Display update information
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
-echo -e " \e[1;97;101m          UPDATE SCRIPT SANAK STORE     \e[0m"
+echo -e " \e[1;97;101m          UPDATE SCRIPT AndyYuda       \e[0m"
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
+echo -e "  \033[1;91m Fix Hitung Akun\033[1;37m"
+echo -e "  \033[1;91m Fix Botssh\033[1;37m"
+echo -e "  \033[1;91m Fix Backup\033[1;37m"
+echo -e "  \033[1;91m Pembaruan Cek Login xray\033[1;37m"
+echo -e "  \033[1;91m Penambahan Port UDP\033[1;37m"
 echo -e "  \033[1;91m update script service\033[1;37m"
+echo -e "----------------------------------------------------------------------"
+echo -e "       Terima Kasih Telah Menggunakan     "
+echo -e "            Premium Script                      "
+echo -e "       RMBL-VPN Tunneling             "
+echo -e "----------------------------------------------------------------------"
+
+# Run the update function with a loading bar
 fun_bar 'res1'
+
+# Final clear screen with information
 echo -e "\033[1;36m━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━\033[0m"
 echo -e ""
-echo -e " Done "
-echo ""
-read -n 1 -s -r -p "Press any key to back on menu"
-menu
-###########- COLOR CODE -##############
